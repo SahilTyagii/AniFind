4f9f28dbdc04ef83869791f69d421674e61571af
SahilTyagii <tyagi28sahil@gmail.com>
2024-06-23 10:07:12 +0530

    projet setup

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import axios from "axios"
+
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
